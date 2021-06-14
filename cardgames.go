31eeb6c9c023df5565c43efd0a13b906de04c709
Manuel Salguero <msalguer@hotmail.com>
2021-06-14 17:10:17 +0000

    Fix for Deploy to work on Heroku.

--- cardgames.go
+++ cardgames.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"encoding/hex"
 	"encoding/json"
-	"flag"
+	//"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
+	//"log"
 	"math/rand"
 	"net/http"
 	"os"
@@ -68,7 +68,7 @@ var readme string
 
 func loadreadme() string {
 	// SHOW README file
-
+/*
 	fptr := flag.String("fpath", "README", "file path to read from")
 	flag.Parse()
 
@@ -92,6 +92,90 @@ func loadreadme() string {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	*/
+
+	readme := `
+
+CARDGAMES (Poker) App README
+----------------------------
+
+Requirements
+
+Golang >=1.6x
+
+Echo Framework
+
+    Installation:
+        cd <PROJECT FOLDER IN $GOPATH>
+        go get -u github.com/labstack/echo
+
+Unzip zip file on a Go directory:
+cd $GOPATH
+mkdir cardgames
+cd cardgames
+
+Run app in script mode:
+go run cardgames.go
+
+Test app:
+go test .
+
+Verbose test app:
+go test . -v
+
+Build app:
+go build
+
+Run (Execute binary):
+./cardgames
+
+===========
+API RESTFUL
+===========
+
+API Help homepage
+=================
+http://domain:8000/api
+
+Example:
+http://127.0.0.1:8000/api
+
+NEW DECK
+========
+
+http://domain:8000/api/deck/:game/new/:shuffle
+
+Game values:
+    poker
+Shuffle values:
+    shuffle OR 1
+    withoutshuffle (default) OR 0
+
+Parameters:
+    ?cards=CODE_CARDS_SEPARATED_COMMAS
+
+Examples:
+http://127.0.0.1:8000/api/deck/poker/new/withoutshuffle
+http://127.0.0.1:8000/api/deck/poker/new/shuffle?cards=AS,KD,AC,2C,KH
+
+OPEN DECK
+=========
+
+http://domain:8000/api/deck/:deck_id
+
+Example:
+http://127.0.0.1:8000/api/deck/a31548c8d6baf5f3987233e38ae81d31
+
+
+DRAW DECK
+=========
+
+http://domain:8000/api/deck/:deck_id/draw?count=number_of_cards_draw
+
+Example:
+http://127.0.0.1:8000/api/deck/a31548c8d6baf5f3987233e38ae81d31/draw?count=5
+`
 	return readme
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
+// For local execution: module cardgames
 module github.com/msalguer/cardgames
 
 go 1.16
